63d8c196b599604b88e8c48ca9ba3d9b007cf19b
Vriz <vrizalas100@hotmail.com>
2016-03-23 02:29:58 +0200

    Update Change_Visibility_Of_Node_In_Database_By_ID.py

--- Change_Visibility_Of_Node_In_Database_By_ID.py
+++ Change_Visibility_Of_Node_In_Database_By_ID.py
@@ -20,35 +20,14 @@ class ChangeVisibilityByID:
     """ Takes an SQL table creates an exact copy and then changes the visibility of certain nodes based on on on the
     users preferences """
 
-    def __init__(self, nodes, cnxn):
+    def __init__(self, nodes, cnxn, lst):
         self.nodes = nodes
         self.cnxn = cnxn
-        self.cond = True
-        self.list_to_change = self.nodes_to_change(self.cond)
-        self.change_visibility_by_id(self.list_to_change)
+        self.lst = lst
+        # Change the visibility of the nodes
+        self.change_visibility_by_id(self.lst)
 
-    def nodes_to_change(self, condition):
-        """This method creates a list with all the Ids of the nodes that
-        the user wants to have their visibility changed
-        @Param condition is boolean variable set to True
-        @Return is a list that contains the Ids of the nodes"""
-
-        # Create an empty list
-        temp_list = []
-        # As long as condition == True
-        while condition:
-            # Promt the user to give us the id of the nodes he wants their visibility to be changed or to end the process
-            temp_id = raw_input("Please type the id of the node or type End to continue! :")
-            # If the users does not want the process to end
-            if temp_id != 'End':
-                # Get the integer of the users input
-                ids = int(temp_id)
-                # Append the list with tha id
-                temp_list.append(ids)
-            # If the user types 'End'
-            else:
-                condition = False
-        return temp_list
+    
 
     def change_visibility_by_id(self, list_0):
         """This method takes a list of ids and sets the visibility of those nodes to zero
@@ -63,8 +42,23 @@ class ChangeVisibilityByID:
 def main():
     # Create a connection to the database and the desired tables
     connection = Connect("NODES")
+    temp_list = []
+    condition = True
+    # As long as condition == True
+    while condition:
+        # Promt the user to give us the id of the nodes he wants their visibility to be changed or to end the process
+        temp_id = raw_input("Please type the id of the node or type End to continue! :")
+        # If the users does not want the process to end
+        if temp_id != 'End':
+            # Get the integer of the users input
+            ids = int(temp_id)
+            # Append the list with tha id
+            temp_list.append(ids)
+        # If the user types 'End'
+        else:
+            condition = False
     # Changes the visibility of certain nodes
-    ChangeVisibilityByID(connection.nodes, connection.cnxn)
+    ChangeVisibilityByID(connection.nodes, connection.cnxn, temp_list)
 
 if __name__ == '__main__':
     main()
